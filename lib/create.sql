7aa220ee888002bfdfea2967c4880879ef581bea
William Pannell <lpann5@gmail.com>
2020-06-24 19:45:14 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, fundinggoal INTEGER, columnb INTEGER);
+CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, fundinggoal INTEGER, startdate INTEGER, enddate INTEGER);
 CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, columnc INTEGER, columnd INTEGER);
 CREATE TABLE pledge (id INTEGER PRIMARY KEY, name TEXT, columne INTEGER, columnf INTEGER);
  
